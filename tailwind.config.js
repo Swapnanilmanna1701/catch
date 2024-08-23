3347d946f424539776a1a725ebc6977956906ea0
Swapnanilmanna1701 <sm.22u10337@btech.nitdgp.ac.in>
2024-08-23 22:03:46 +0530

    setting up

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+
+module.exports = {
+   content: [],
+   content: ["./App.{js,jsx,ts,tsx}", "./<custom directory>/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
